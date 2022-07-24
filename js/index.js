9ee6529c1f6787df5fd31971df45af60db3ea42b
Nayetta <noemi.silvestre@gmail.com>
2022-07-24 21:10:38 +0200

    interaction2 commit

--- js/index.js
+++ js/index.js
@@ -1,10 +1,19 @@
 console.log("I'm ready!");
 
 // Iteration 1: Names and Input
+
 let hacker1 = "Noemi";
 let hacker2 = "Noemi també";
 console.log("The driver's name is " + hacker1);
 console.log("The navigator's name is " + hacker2);
-// Iteration 2: Conditionals
 
+// Iteration 2: Conditionals
+let driver = hacker1.length;
+let navigator = hacker2.length;
+
+console.log(`The driver has the longest name, it has ${driver} characters.`);
+console.log(
+  `It seems that the navigator has the longest name, it has ${navigator} characters.`
+);
+console.log(`Wow, you both have equally long names, ${driver} characters!`);
 // Iteration 3: Loops
