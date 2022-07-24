769bf2bdc2219cd12cd731071afb9f25c08eded0
Nayetta <noemi.silvestre@gmail.com>
2022-07-24 20:56:39 +0200

    interaction 1 commit

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
-// Iteration 1: Names and Input
-//
-// Iteration 2: Conditionals
+console.log("I'm ready!");
 
+// Iteration 1: Names and Input
+let hacker1 = "Noemi";
+let hacker2 = "Noemi també";
+console.log("The driver's name is " + hacker1);
+console.log("The navigator's name is " + hacker2);
+// Iteration 2: Conditionals
 
 // Iteration 3: Loops
