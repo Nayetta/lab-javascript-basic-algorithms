0dbc505b22935f845bdd86189355cae7d5345ffe
Nayetta <noemi.silvestre@gmail.com>
2022-07-24 23:50:33 +0200

    integracion 3 casi

--- js/index.js
+++ js/index.js
@@ -8,12 +8,45 @@ console.log("The driver's name is " + hacker1);
 console.log("The navigator's name is " + hacker2);
 
 // Iteration 2: Conditionals
+
 let driver = hacker1.length;
 let navigator = hacker2.length;
 
-console.log(`The driver has the longest name, it has ${driver} characters.`);
-console.log(
-  `It seems that the navigator has the longest name, it has ${navigator} characters.`
-);
-console.log(`Wow, you both have equally long names, ${driver} characters!`);
+if (driver > navigator) {
+  console.log(`The driver has the longest name, it has ${driver} characters.`);
+} else if (driver < navigator) {
+  console.log(
+    `It seems that the navigator has the longest name, it has ${navigator} characters.`
+  );
+} else if (driver === navigator) {
+  console.log(`Wow, you both have equally long names, ${driver} characters!`);
+} else {
+  console.log("hey... ehhhm... something happened...");
+}
+
 // Iteration 3: Loops
+// resultat: el nom del driver ha de sortir lletra a lletra i en majuscula
+let capitals = "";
+for (let i = 0; i < hacker1.length; i++) {
+  capitals = capitals + " " + hacker1[i];
+}
+console.log(capitals.toUpperCase());
+
+// resultat: el nom del navegator ha de sortir del reves
+
+let reverse = "";
+for (let i = 0; i < hacker2.length; i++) {
+  reverse = hacker2[i] + reverse;
+}
+console.log(reverse);
+
+//comparació per ordre alfabetic
+
+let team = [capitals, reverse];
+if (team.sort == [capitals, reverse]) {
+  console.log("The driver's name goes first.");
+} else if (team.sort == [reverse, capitals]) {
+  console.log("Yo, the navigator goes first definitely");
+} else {
+  console.log("What?! You both have the same name?");
+}
